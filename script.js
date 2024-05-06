ebebe641244fb1f859c82cc7aa7b0952ea96b59c
javohirgvdev <gayratovjavohir90@gmail.com>
2024-05-06 21:03:07 +0500

    I added html css and js

---
+++ script.js
@@ -0,0 +1,56 @@
+const todoInput = document.querySelector(".todo-input");
+const todoBtn = document.querySelector(".todo-btn");
+const todoList = document.querySelector(".todo-list");
+const todoForm = document.querySelector("form");
+
+todoBtn.addEventListener("click", addTodo);
+
+function addTodo(event) {
+    event.preventDefault();
+
+    const todoDiv = document.createElement("div")
+    const newDiv = document.createElement("div")
+    const newTodo = document.createElement("li");
+
+    todoDiv.classList.add("todo-div");
+    newDiv.classList.add("new-div");
+    newTodo.classList.add("todo-item");
+    
+    todoList.appendChild(todoDiv);
+    todoDiv.appendChild(newTodo);
+    todoDiv.appendChild(newDiv)
+    newTodo.innerHTML = todoInput.value;
+
+
+    // const todoCheck = document.createElement("button")
+    // todoCheck.innerHTML = '<i class="fa-solid fa-check"></i>';
+    // todoCheck.classList.add("todo-check")
+
+    // newDiv.appendChild(todoCheck)
+
+
+    const todoTrash = document.createElement("button")
+    todoTrash.innerHTML = '<i class="fa-solid fa-trash"></i>';
+    todoTrash.classList.add("todo-trash")
+
+    newDiv.appendChild(todoTrash)
+
+
+    todoTrash.addEventListener("click", trashEvent)
+
+    function trashEvent(event) {
+        event.preventDefault();
+        
+        todoDiv.remove();
+    }
+
+    // todoCheck.addEventListener("click", checkEvent)
+
+    // function checkEvent(event) {
+    //     event.preventDefault();
+
+    //     todoDiv.classList.toggle("completed");
+    // }
+
+    todoInput.value = "";
+}
